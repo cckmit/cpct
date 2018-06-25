76e95829d993309a02aaf5e4cce249d255d20c89
linchao <1102171559@qq.com>
2018-06-25 19:27:17 +0800

    Add 活动

---
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/campaign/MktCampaignService.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)MktCampaignService.java, 2018/6/22.
+ * <p/>
+ * Copyright 2018 Netease, Inc. All rights reserved.
+ * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.zjtelcom.cpct.service.campaign;
+
+import com.zjtelcom.cpct.domain.campaign.DTO.MktCampaign;
+
+/**
+ * Description:
+ * author: linchao
+ * date: 2018/06/22 22:21
+ * version: V1.0
+ */
+public interface MktCampaignService {
+
+    public int saveMktCampaign(MktCampaign mktCampaign);
+
+}
