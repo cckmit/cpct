c87ffa33d49fb418d421c2597633589338138485
11470847@qq.com <11470847@qq.com>
2018-07-05 10:48:35 +0800

    登录接口修改

--- cpct-web/src/main/java/com/zjtelcom/cpct/controller/system/LoginController.java
+++ cpct-web/src/main/java/com/zjtelcom/cpct/controller/system/LoginController.java
@@ -1,7 +1,14 @@
 package com.zjtelcom.cpct.controller.system;
 
 import com.zjtelcom.cpct.controller.BaseController;
+import com.zjtelcom.cpct.dao.system.SysMenuMapper;
+import com.zjtelcom.cpct.dao.system.SysRoleMapper;
+import com.zjtelcom.cpct.domain.system.SysMenu;
+import com.zjtelcom.cpct.domain.system.SysRole;
+import com.zjtelcom.cpct.domain.system.SysStaff;
+import com.zjtelcom.cpct.domain.system.UserInfo;
 import com.zjtelcom.cpct.service.system.SysStaffService;
+import com.zjtelcom.cpct.util.CopyPropertiesUtil;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
@@ -10,26 +17,40 @@ import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class LoginController extends BaseController {
 
     @Autowired
     private SysStaffService sysStaffService;
 
+    @Autowired
+    private SysMenuMapper sysMenuMapper;
+
+    @Autowired
+    private SysRoleMapper sysRoleMapper;
+
     /**
      * 登录
      *
-     * @param username 用户名
-     * @param password 密码
+     * @param params
      * @return
      */
     @RequestMapping(value = "login", method = RequestMethod.POST)
     @ResponseBody
-    public String doLogin(@RequestParam("username") String username,
-                          @RequestParam("password") String password) {
+    public String doLogin(@RequestBody Map<String, String> params) {
+
+        String username = params.get("username");
+        String password = params.get("password");
 
         // 获取当前的Subject
         Subject currentUser = SecurityUtils.getSubject();
+
+        Map<String, Object> result = new HashMap<>();
 
         // 测试当前的用户是否已经被认证，即是否已经登陆
         // 调用Subject的isAuthenticated
@@ -41,16 +62,37 @@ public class LoginController extends BaseController {
                 // 执行登陆
                 currentUser.login(token);
 
+                SysStaff sysStaff = (SysStaff) SecurityUtils.getSubject().getPrincipal();
+                UserInfo userInfo = new UserInfo();
+                CopyPropertiesUtil.copyBean2Bean(userInfo, sysStaff);
+                //查询用户角色
+                List<SysRole> listRole = sysRoleMapper.selectByStaffId(userInfo.getStaffId());
+                userInfo.setRoleList(listRole);
+                //查询用户菜单
+                List<SysMenu> menuList = new ArrayList<>();
+                for(SysRole sysRole : listRole) {
+                    menuList = sysMenuMapper.selectByRoleId(sysRole.getRoleId());
+                }
+                userInfo.setMenuList(menuList);
+                result.put("user", userInfo);
+
                 //记录最后一次登录时间
                 sysStaffService.lastLogin(username);
+
             } catch (AuthenticationException ae) {
                 ae.printStackTrace();
-                logger.info("登录失败 = {}",ae.getMessage());
-                return initFailRespInfo("登录失败","0001");
+                logger.info("登录失败 = {}", ae.getMessage());
+                return initFailRespInfo("登录失败", "0001");
+            } catch (Exception e) {
+                e.printStackTrace();
             }
+        } else {
+            SysStaff sysStaff = (SysStaff) SecurityUtils.getSubject().getPrincipal();
+            result.put("user", sysStaff);
+            result.put("menu", sysStaff);
         }
 
-        return initSuccRespInfo(null);
+        return initSuccRespInfo(result);
     }
 
     /**
