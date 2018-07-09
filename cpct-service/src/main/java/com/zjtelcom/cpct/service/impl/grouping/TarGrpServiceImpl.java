37435f5ddaf482994030fb4fbedb3175de616120
彭羽 <yannisPeng@gmail.com>
2018-07-09 15:33:38 +0800

    1.策略试运算

--- cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TarGrpServiceImpl.java
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TarGrpServiceImpl.java
@@ -38,7 +38,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
