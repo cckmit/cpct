7c9ef7bbe9dda39e091080c6c48a2a1606d57bc2
linchao <1102171559@qq.com>
2018-08-09 20:41:43 +0800

    dubbo服务启动

---
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/DubboApplication.java
@@ -0,0 +1,30 @@
+package com.zjtelcom.cpct;
+
+/**
+ * Created by huanghua on 2017/5/23.
+ */
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(value={"com.zjtelcom.cpct"})
+@MapperScan("com.zjtelcom.cpct.dao")
+@EnableTransactionManagement
+public class DubboApplication {
+
+    public static Logger logger = LoggerFactory.getLogger(DubboApplication.class);
+    // for local development, debug in IDE
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DubboApplication.class, args);
+        logger.info("***********CPCT*********启动**********");
+    }
+}
+
