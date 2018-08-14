a6ffa49aa40eda1f41afbfdf6c3bb5e566ff4476
11470847@qq.com <11470847@qq.com>
2018-08-14 16:59:51 +0800

    添加es模块

---
+++ cpct-es/src/main/java/com/zjtelcom/cpct/elastic/config/IndexList.java
@@ -0,0 +1,37 @@
+package com.zjtelcom.cpct.elastic.config;/**
+ * Created by elbert on 2018/8/2.
+ */
+
+/**
+ * @Author:elbert
+ * @Date:2018-08-02
+ */
+
+public class IndexList {
+
+    /**
+     * 事件模块索引
+     */
+    public static String EVENT_MODULE="event_module";
+
+    /**
+     * 标签模块索引
+     */
+    public static String  Label_MODULE="label_module";
+    /**
+     * 活动信息模块索引
+     */
+    public static String  ACTIVITY_MODULE="activity_module";
+
+    /**
+     * 规则模块索引
+     */
+    public static String  RULE_MODULE="rule_module";
+
+    /**
+     * 策略模块
+     */
+    public static String  STRATEGY_MODULE="strategy_module";
+
+
+}
