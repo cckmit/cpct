07d88a6f477d83020af360b7ddf396b1186034ce
linchao <1102171559@qq.com>
2018-08-21 18:52:30 +0800

    事件源

--- cpct-common/src/main/java/com/zjtelcom/cpct/dto/event/EventSorce.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dto/event/EventSorce.java
@@ -4,4 +4,22 @@ import com.zjtelcom.cpct.domain.event.EventSorceDO;
 
 public class EventSorce extends EventSorceDO {
 
+    private String createStaffName; //创建人名称
+    private String updateStaffName; //更新人名称
+
+    public String getCreateStaffName() {
+        return createStaffName;
+    }
+
+    public void setCreateStaffName(String createStaffName) {
+        this.createStaffName = createStaffName;
+    }
+
+    public String getUpdateStaffName() {
+        return updateStaffName;
+    }
+
+    public void setUpdateStaffName(String updateStaffName) {
+        this.updateStaffName = updateStaffName;
+    }
 }
