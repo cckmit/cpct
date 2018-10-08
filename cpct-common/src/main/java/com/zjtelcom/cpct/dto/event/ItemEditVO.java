0a9755f9ce02e1b7e9827387a59f8f0564d8a52a
侯云峰 <992624275@qq.com>
2018-10-08 15:27:59 +0800

    xiaoshoupin

--- cpct-common/src/main/java/com/zjtelcom/cpct/dto/event/ItemEditVO.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dto/event/ItemEditVO.java
@@ -7,6 +7,15 @@ public class ItemEditVO implements Serializable {
     private String evtItemName;//记录事件采集项的名称
     private String evtItemCode;//记录事件采集项的编码，主要用于格式化
     private String evtItemDesc;//采集项描述
+    private String isLabel;
+
+    public String getIsLabel() {
+        return isLabel;
+    }
+
+    public void setIsLabel(String isLabel) {
+        this.isLabel = isLabel;
+    }
 
     public Long getEvtItemId() {
         return evtItemId;
