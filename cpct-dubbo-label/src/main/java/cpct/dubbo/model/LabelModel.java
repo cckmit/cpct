63f8443a4997aea625120daba9436384d8c13db8
侯云峰 <992624275@qq.com>
2018-10-15 15:00:48 +0800

    xiaoshoupin

--- cpct-dubbo-label/src/main/java/cpct/dubbo/model/LabelModel.java
+++ cpct-dubbo-label/src/main/java/cpct/dubbo/model/LabelModel.java
@@ -5,21 +5,8 @@ public class LabelModel {
     private Long ctasTableDefinitionRowId;//源表定义主键
     private String tagName;//标签名称
     private String sourceTableColumnName;//源表字段
-    private String sourceTableColumnType;//标签字段类型
-    private String usedStatus;//
-    private String sensitiveColumnFlag;//敏感字段标志 1：敏感字段，2：不敏感字段
-    private String sensitiveType;//脱敏类型 1：全部打*（适用地址等字段），2、首字符除外打*（适用姓名等字段）;3、中间5位打*（适用手机等字段）;4、中间9位打*（适用身份证号等字段）;
-    private String fitDomain;//适用域
-    private String showFlag;//
+    private String labState;//上线：3；下线：5
 
-
-    public String getShowFlag() {
-        return showFlag;
-    }
-
-    public void setShowFlag(String showFlag) {
-        this.showFlag = showFlag;
-    }
 
     public Long getTagRowId() {
         return tagRowId;
@@ -53,43 +40,11 @@ public class LabelModel {
         this.sourceTableColumnName = sourceTableColumnName;
     }
 
-    public String getSourceTableColumnType() {
-        return sourceTableColumnType;
+    public String getLabState() {
+        return labState;
     }
 
-    public void setSourceTableColumnType(String sourceTableColumnType) {
-        this.sourceTableColumnType = sourceTableColumnType;
-    }
-
-    public String getUsedStatus() {
-        return usedStatus;
-    }
-
-    public void setUsedStatus(String usedStatus) {
-        this.usedStatus = usedStatus;
-    }
-
-    public String getSensitiveColumnFlag() {
-        return sensitiveColumnFlag;
-    }
-
-    public void setSensitiveColumnFlag(String sensitiveColumnFlag) {
-        this.sensitiveColumnFlag = sensitiveColumnFlag;
-    }
-
-    public String getSensitiveType() {
-        return sensitiveType;
-    }
-
-    public void setSensitiveType(String sensitiveType) {
-        this.sensitiveType = sensitiveType;
-    }
-
-    public String getFitDomain() {
-        return fitDomain;
-    }
-
-    public void setFitDomain(String fitDomain) {
-        this.fitDomain = fitDomain;
+    public void setLabState(String labState) {
+        this.labState = labState;
     }
 }
--- cpct-dubbo-label/src/main/java/cpct/dubbo/service/impl/SyncLabelServiceImpl.java
+++ cpct-dubbo-label/src/main/java/cpct/dubbo/service/impl/SyncLabelServiceImpl.java
@@ -2,14 +2,27 @@ package cpct.dubbo.service.impl;
 
 import com.zjtelcom.cpct.dao.channel.InjectionLabelMapper;
 import com.zjtelcom.cpct.dao.channel.InjectionLabelValueMapper;
+import com.zjtelcom.cpct.domain.channel.Label;
+import com.zjtelcom.cpct.domain.channel.LabelValue;
+import com.zjtelcom.cpct.enums.Operator;
+import com.zjtelcom.cpct.util.BeanUtil;
+import com.zjtelcom.cpct.util.ChannelUtil;
+import com.zjtelcom.cpct.util.UserUtil;
+import cpct.dubbo.model.LabelModel;
+import cpct.dubbo.model.LabelValueModel;
 import cpct.dubbo.model.RecordModel;
 import cpct.dubbo.service.SyncLabelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
+import java.util.*;
+
+import static com.zjtelcom.cpct.constants.CommonConstant.CODE_FAIL;
+import static com.zjtelcom.cpct.constants.CommonConstant.CODE_SUCCESS;
+import static com.zjtelcom.cpct.constants.CommonConstant.STATUSCD_EFFECTIVE;
 
 
 /**
@@ -21,15 +34,165 @@ import java.util.Map;
 @Service
 public class SyncLabelServiceImpl  implements SyncLabelService {
     public static final Logger logger = LoggerFactory.getLogger(SyncLabelServiceImpl.class);
-
     @Autowired
     private InjectionLabelMapper labelMapper;
     @Autowired
     private InjectionLabelValueMapper labelValueMapper;
 
 
+    /**
+     * 标签同步对外接口
+     * @param record
+     * @return
+     */
     @Override
+    @Transactional
     public Map<String, Object> syncLabelInfo(RecordModel record) {
-        return null;
+        Map<String,Object> result = new HashMap<>();
+        try {
+            switch (record.getTag().getLabState()){
+                case "3":
+                    result = addLabel(record);
+                    break;
+                case "5":
+                    result = updateLabel(record);
+                    break;
+            }
+        }catch (Exception e){
+            logger.error("[op:SyncLabelServiceImpl] fail to syncLabelInfo",e);
+            result.put("resultCode",CODE_FAIL);
+            result.put("resultMsg"," fail to syncLabelInfo");
+            return result;
+        }
+        return result;
+    }
+
+
+    /**
+     * 新增标签
+     * @param record
+     * @return
+     */
+    private Map<String,Object> addLabel(RecordModel record){
+        Map<String,Object> result = new HashMap<>();
+        LabelModel tagModel = record.getTag();
+        Label labelValodate = labelMapper.selectByTagRowId(record.getTag().getTagRowId());
+        if (labelValodate!=null){
+            result.put("resultCode",CODE_FAIL);
+            result.put("resultMsg","标签已存在，请选择更新或删除后添加");
+            return result;
+        }
+        List<LabelValueModel> valueModelList = new ArrayList<>();
+        if (record.getTagValueList()!=null && !record.getTagValueList().isEmpty()){
+            valueModelList = record.getTagValueList();
+        }
+        Label label = new Label();
+        label.setTagRowId(tagModel.getTagRowId());
+        //todo
+        label.setLabelDataType("");
+        label.setInjectionLabelCode(tagModel.getSourceTableColumnName());
+        label.setInjectionLabelName(tagModel.getTagName());
+        label.setInjectionLabelDesc(tagModel.getTagName());
+        label.setStatusCd(STATUSCD_EFFECTIVE);
+        label.setCreateStaff(UserUtil.loginId());
+        label.setCreateDate(new Date());
+        //todo
+        label.setConditionType("");
+        //todo 标签操作符 对应显示类型
+        label.setOperator(String.valueOf(Operator.EQUAL.getValue()));
+        label.setScope(1);
+        label.setIsShared(0);
+        //todo 暂无标签类型
+        label.setLabelType("1000");
+        //todo 暂无值类型
+        if (record.getTagValueList()!=null && !record.getTagValueList().isEmpty()){
+            label.setLabelValueType("2000");
+        }else {
+            label.setLabelValueType("1000");
+        }
+//        label.setLabelDataType(ChannelUtil.getDataType(tagModel.getSourceTableColumnType()));
+        label.setCreateDate(new Date());
+        labelMapper.insert(label);
+        result.put("resultCode",CODE_SUCCESS);
+        result.put("resultMsg","新增成功");
+        return result;
+    }
+
+    /**
+     * 更新标签
+     * @param record
+     * @return
+     */
+    private Map<String,Object> updateLabel(RecordModel record){
+        Map<String,Object> result = new HashMap<>();
+        //todo 先删除再添加
+        LabelModel tagModel = record.getTag();
+        Label label = labelMapper.selectByTagRowId(tagModel.getTagRowId());
+        if (label==null){
+            result.put("resultCode",CODE_FAIL);
+            result.put("resultMsg","未找到对应标签");
+            return result;
+        }
+        List<LabelValue> valueList = labelValueMapper.selectByLabelId(label.getInjectionLabelId());
+        if (!valueList.isEmpty()){
+            for (LabelValue value : valueList){
+                labelValueMapper.deleteByPrimaryKey(value.getLabelValueId());
+            }
+        }
+        labelMapper.deleteByPrimaryKey(label.getInjectionLabelId());
+        addLabel(record);
+        result.put("resultCode",CODE_SUCCESS);
+        result.put("resultMsg","更新成功");
+        return result;
+    }
+
+    /**
+     * 删除标签
+     * @param record
+     * @return
+     */
+    private Map<String,Object> deleteLabel(RecordModel record){
+        Map<String,Object> result = new HashMap<>();
+        LabelModel tagModel = record.getTag();
+        Label label = labelMapper.selectByTagRowId(tagModel.getTagRowId());
+        if (label==null){
+            result.put("resultCode",CODE_FAIL);
+            result.put("resultMsg","未找到对应标签");
+            return result;
+        }
+        List<LabelValue> valueList = labelValueMapper.selectByLabelId(label.getInjectionLabelId());
+        if (!valueList.isEmpty()){
+            for (LabelValue value : valueList){
+                labelValueMapper.deleteByPrimaryKey(value.getLabelValueId());
+            }
+        }
+        labelMapper.deleteByPrimaryKey(label.getInjectionLabelId());
+        result.put("resultCode",CODE_SUCCESS);
+        result.put("resultMsg","删除成功");
+        return result;
+    }
+
+
+    private Map<String,Object> syncLabelValue(List<LabelValueModel> valueModelList,Long labelId) {
+        Map<String,Object> result = new HashMap<>();
+        List<LabelValue> valueList = new ArrayList<>();
+        List<String> stringList = new ArrayList<>();
+        for (LabelValueModel info : valueModelList){
+            LabelValue value = BeanUtil.create(info,new LabelValue());
+            value.setInjectionLabelId(labelId);
+            value.setValueDesc(info.getTagValueName());
+            value.setValueName(info.getTagValueName());
+            value.setLabelValue(info.getTagDownValue());
+            value.setCreateDate(new Date());
+            value.setStatusCd("1000");
+            value.setUpdateDate(new Date());
+            valueList.add(value);
+            stringList.add(value.getLabelValue());
+            labelValueMapper.insert(value);
+        }
+        result.put("resultCode",CODE_SUCCESS);
+        result.put("resultMsg","标签值同步成功");
+        result.put("valueString",ChannelUtil.StringList2String(stringList));
+        return result;
     }
 }
