3449a849298f3b3acea6642ba6922d102208b685
侯云峰 <992624275@qq.com>
2018-10-15 11:23:17 +0800

    xiaoshoupin

---
+++ cpct-dubbo-label/src/main/java/cpct/dubbo/service/impl/SyncLabelServiceImpl.java
@@ -0,0 +1,24 @@
+package cpct.dubbo.service.impl;
+
+import cpct.dubbo.service.SyncLabelService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+
+
+/**
+ * Description:
+ * author: hyf
+ * date: 2018/07/17 11:11
+ * version: V1.0
+ */
+@Service
+public class SyncLabelServiceImpl  implements SyncLabelService {
+    public static final Logger logger = LoggerFactory.getLogger(SyncLabelServiceImpl.class);
+
+
+
+
+
+}
