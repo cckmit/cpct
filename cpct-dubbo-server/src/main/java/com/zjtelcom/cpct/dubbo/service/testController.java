a1b328b0751bfdcb85c5101e7aa8a28ca47b0241
linchao <1102171559@qq.com>
2018-10-19 10:36:54 +0800

    活动事件关联bug

--- cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/testController.java
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/testController.java
@@ -1,32 +1,32 @@
-package com.zjtelcom.cpct.dubbo.service;
-
-import com.zjtelcom.cpct.dubbo.model.FilterRuleInputReq;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
-
-@RestController
-@RequestMapping("/test")
-public class testController  {
-    @Autowired
-    private ChannelService channelService;
-
-
-    @RequestMapping("getChannelDetail")
-    public Map<String, Object> getChannelDetail(String channelCode) {
-        return channelService.getChannelDetail(channelCode);
-    }
-
-    @RequestMapping("getEventDetail")
-    public Map<String, Object> getEventDetail(String evtCode){
-        return channelService.getEventDetail(evtCode);
-    }
-
-
-    @RequestMapping("importRuleUserList")
-    public Map<String, Object> importRuleUserList(FilterRuleInputReq req) {
-        return channelService.importRuleUserList(req);
-    }
-}
+//package com.zjtelcom.cpct.dubbo.service;
+//
+//import com.zjtelcom.cpct.dubbo.model.FilterRuleInputReq;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/test")
+//public class testController  {
+//    @Autowired
+//    private ChannelService channelService;
+//
+//
+//    @RequestMapping("getChannelDetail")
+//    public Map<String, Object> getChannelDetail(String channelCode) {
+//        return channelService.getChannelDetail(channelCode);
+//    }
+//
+//    @RequestMapping("getEventDetail")
+//    public Map<String, Object> getEventDetail(String evtCode){
+//        return channelService.getEventDetail(evtCode);
+//    }
+//
+//
+//    @RequestMapping("importRuleUserList")
+//    public Map<String, Object> importRuleUserList(FilterRuleInputReq req) {
+//        return channelService.importRuleUserList(req);
+//    }
+//}
