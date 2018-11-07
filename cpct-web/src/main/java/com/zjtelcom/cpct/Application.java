86acaa51341125742cac9e21f09ad2f57df2cc1e
侯云峰 <992624275@qq.com>
2018-11-07 10:07:38 +0800

    xiaoshoupin

--- cpct-web/src/main/java/com/zjtelcom/cpct/Application.java
+++ cpct-web/src/main/java/com/zjtelcom/cpct/Application.java
@@ -4,6 +4,14 @@ package com.zjtelcom.cpct;
  * Created by huanghua on 2017/5/23.
  */
 
+import com.ctzj.smt.bss.centralized.authenticate.config.HttpSessionConfig;
+import com.ctzj.smt.bss.centralized.authenticate.config.WebSecurityConfig;
+import com.ctzj.smt.bss.centralized.authenticate.security.userdetails.service.MyUserDetailsService;
+import com.ctzj.smt.bss.centralized.exception.MyBasicErrorController;
+import com.ctzj.smt.bss.centralized.exception.RestExceptionHandler;
+import com.ctzj.smt.bss.centralized.web.advice.MyResponseBodyAdvice;
+import com.ctzj.smt.bss.centralized.web.config.MyFilterConfig;
+import com.ctzj.smt.bss.centralized.web.config.MyJacksonConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.slf4j.Logger;
@@ -15,6 +23,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -25,8 +34,17 @@ import org.springframework.web.client.RestTemplate;
 //@EnableAutoConfiguration
 
 
+
 //@ImportResource({ "classpath:dubbo/*.xml","classpath:task/*.xml" })
 //@ImportResource({ "classpath:dubbo/*.xml"})
+@Import({WebSecurityConfig.class,
+        HttpSessionConfig.class,
+        MyUserDetailsService.class,
+        MyResponseBodyAdvice.class,
+        RestExceptionHandler.class,
+        MyBasicErrorController.class,
+        MyFilterConfig.class,
+        MyJacksonConfig.class})
 @ComponentScan(value={"com.zjtelcom.cpct"})
 @EnableTransactionManagement
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class, MybatisAutoConfiguration.class})
