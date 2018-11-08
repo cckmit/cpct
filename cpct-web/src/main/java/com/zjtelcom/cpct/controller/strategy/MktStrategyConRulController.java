bb14175b409e5075c80c139475170a3a2bb5013e
linchao <1102171559@qq.com>
2018-11-08 19:03:10 +0800

    策略批量工具

--- cpct-web/src/main/java/com/zjtelcom/cpct/controller/strategy/MktStrategyConRulController.java
+++ cpct-web/src/main/java/com/zjtelcom/cpct/controller/strategy/MktStrategyConRulController.java
@@ -144,4 +144,53 @@ public class MktStrategyConRulController {
         Map<String, Object> map = mktStrategyConfRuleService.deleteTarGrpBatch(ruleIdList, tarGrpNewId.longValue());
         return JSON.toJSONString(map);
     }
+
+
+
+
+    /**
+     * 批量插入客户分群
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/insertCamItemBatch", method = RequestMethod.POST)
+    @CrossOrigin
+    public String insertCamItemBatch(@RequestBody Map<String, Object> params){
+        List<Integer> ruleIdList = (List<Integer>) params.get("ruleIdList");
+        List<Integer> camitemIdList = (List<Integer>) params.get("camitemIdList");
+        // 查询新的客户分群
+        Map<String, Object> map = mktStrategyConfRuleService.insertCamItemBatch(ruleIdList, camitemIdList);
+        return JSON.toJSONString(map);
+    }
+
+
+    /**
+     * 批量修改客户分群
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/updateCamItemBatch", method = RequestMethod.POST)
+    @CrossOrigin
+    public String updateCamItemBatch(@RequestBody Map<String, Object> params){
+        List<Integer> ruleIdList = (List<Integer>) params.get("ruleIdList");
+        List<Integer> camitemIdList = (List<Integer>) params.get("camitemIdList");
+        // 查询新的客户分群
+        Map<String, Object> map = mktStrategyConfRuleService.updateCamItemBatch(ruleIdList, camitemIdList);
+        return JSON.toJSONString(map);
+    }
+
+    /**
+     * 批量删除客户分群
+     * @param params
+     * @return
+     */
+    @RequestMapping(value = "/deleteCamItemBatch", method = RequestMethod.POST)
+    @CrossOrigin
+    public String deleteCamItemBatch(@RequestBody Map<String, Object> params){
+        List<Integer> ruleIdList = (List<Integer>) params.get("ruleIdList");
+        List<Integer> camitemIdList = (List<Integer>) params.get("camitemIdList");
+        // 查询新的客户分群
+        Map<String, Object> map = mktStrategyConfRuleService.deleteCamItemBatch(ruleIdList, camitemIdList);
+        return JSON.toJSONString(map);
+    }
 }
