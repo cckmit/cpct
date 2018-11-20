5567e050b8b7118650a3934edb40d2e0a9d23c57
gqa <657182420@qq.com>
2018-11-20 20:04:37 +0800

    ddd

---
+++ cpct-common/src/main/java/com/zjtelcom/cpct_prod/dao/offer/OfferProdMapper.java
@@ -0,0 +1,35 @@
+package com.zjtelcom.cpct_prod.dao.offer;
+
+
+import com.zjtelcom.cpct.domain.channel.Offer;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface OfferProdMapper {
+    int deleteByPrimaryKey(Integer offerId);
+
+    int insert(Offer record);
+
+    Offer selectByPrimaryKey(Integer offerId);
+
+    List<Offer> selectAll();
+
+    Offer selectByCamItemId(Long camItemId);
+
+    List<Offer> findByName(@Param("offerName") String name);
+
+    List<Offer> selectByCode(@Param("offerCode") String code);
+
+    Offer selectByPrimaryKeyAndName(@Param("offerId") Integer offerId, @Param("productName") String productName);
+
+    List<Offer> listByCatalogItemId(@Param("catalogId") Long catalogId);
+
+    List<String> listByOfferIdList(@Param("list") List<Long> offerList);
+
+    int updateByPrimaryKey(Offer record);
+}
