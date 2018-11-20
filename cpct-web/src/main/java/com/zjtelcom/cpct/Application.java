05ecfd391905852725fa383174aafa77218c36c9
11470847@qq.com <11470847@qq.com>
2018-11-20 17:59:36 +0800

    修改cpct实时接口

--- cpct-web/src/main/java/com/zjtelcom/cpct/Application.java
+++ cpct-web/src/main/java/com/zjtelcom/cpct/Application.java
@@ -3,10 +3,9 @@ package com.zjtelcom.cpct;
 /**
  * Created by huanghua on 2017/5/23.
  */
-//import com.ctzj.smt.bss.centralized.authenticate.config.HttpSessionConfig;
-//import com.ctzj.smt.bss.centralized.authenticate.config.WebSecurityConfig;
-//import com.ctzj.smt.bss.centralized.authenticate.security.userdetails.service.MyUserDetailsService;
-//import com.ctzj.smt.bss.centralized.web.config.MyFilterConfig;
+import com.ctzj.smt.bss.centralized.authenticate.config.HttpSessionConfig;
+import com.ctzj.smt.bss.centralized.authenticate.config.WebSecurityConfig;
+import com.ctzj.smt.bss.centralized.authenticate.security.userdetails.service.MyUserDetailsService;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +25,7 @@ import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ImportResource("classpath:dubbo/dubbo-${spring.profiles.active}.xml")
-//@Import({WebSecurityConfig.class,HttpSessionConfig.class,MyUserDetailsService.class})
+@Import({WebSecurityConfig.class,HttpSessionConfig.class,MyUserDetailsService.class})
 @ComponentScan(value={"com.zjtelcom.cpct"})
 @EnableTransactionManagement
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class,DataSourceTransactionManagerAutoConfiguration.class, MybatisAutoConfiguration.class})
