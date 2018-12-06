87d637b99b3c2933d14a40afd52cae3097746865
编织者 <xialei003>
2018-12-06 19:13:18 +0800

    服务代码

---
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dao/channel/ServiceMapper.java
@@ -0,0 +1,20 @@
+package com.zjtelcom.cpct.dao.channel;
+
+import com.zjtelcom.cpct.domain.channel.ServiceEntity;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ServiceMapper {
+    int deleteByPrimaryKey(Long serviceId);
+
+    int insert(ServiceEntity record);
+
+    ServiceEntity selectByPrimaryKey(Long mktResId);
+
+    List<ServiceEntity> selectAll();
+
+    int updateByPrimaryKey(ServiceEntity record);
+
+    List<ServiceEntity> selectByServiceName(@Param("serviceName") String serviceName);
+}
---
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/channel/ServiceService.java
@@ -0,0 +1,7 @@
+package com.zjtelcom.cpct.service.channel;
+
+import java.util.Map;
+
+public interface ServiceService {
+    public Map<String, Object> getServiceListByName(Map<String,Object> params);
+}
