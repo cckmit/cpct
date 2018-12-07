6fbe2ffa1cae34550121654ead52075282dfa092
高琼安 <657182420@qq.com>
2018-12-07 17:52:49 +0800

    !930 集团redis配置改造，事件命中增加事件规则,活动同步大数据接口打开 Merge pull request !930 from 高琼安/dev_cpct_20180824_gqa

--- cpct-cache/src/main/java/com/zjtelcom/cpct/config/RedisConfig.java
+++ cpct-cache/src/main/java/com/zjtelcom/cpct/config/RedisConfig.java
@@ -1,8 +1,11 @@
 package com.zjtelcom.cpct.config;
 
+import com.ctg.itrdc.cache.pool.CtgJedisPool;
+import com.ctg.itrdc.cache.pool.CtgJedisPoolConfig;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
@@ -14,8 +17,12 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description reids配置
@@ -86,5 +93,37 @@ public class RedisConfig extends CachingConfigurerSupport {
         template.setValueSerializer(jackson2JsonRedisSerializer);
     }
 
+
+    @Value("${redisConfig.ip}")
+    private  String redisIp;
+    @Value("${redisConfig.port}")
+    private  Integer redisPort;
+    @Value("${redisConfig.database}")
+    private  Integer redisDataBase;
+    @Value("${redisConfig.password}")
+    private  String redisPassword;
+
+
+    /**
+     * 集团定制redis注入
+     * @return
+     */
+    @Bean
+    public CtgJedisPool ctgJedisPool(){
+        List<HostAndPort> hostAndPortList = new ArrayList();
+        // 接入机的ip和端口号
+        HostAndPort host = new HostAndPort(redisIp, redisPort);
+        hostAndPortList.add(host);
+
+        GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(5); //最大空闲连接数
+        poolConfig.setMaxTotal(10); // 最大连接数（空闲+使用中），不超过应用线程数，建议为应用线程数的一半
+        poolConfig.setMinIdle(5); //保持的最小空闲连接数
+        poolConfig.setMaxWaitMillis(3000000);
+        CtgJedisPoolConfig config = new CtgJedisPoolConfig(hostAndPortList);
+        config.setDatabase(redisDataBase).setPassword(redisPassword).setPoolConfig(poolConfig).setPeriod(1000).setMonitorTimeout(100);
+
+        return  new CtgJedisPool(config);
+    }
 
 }
