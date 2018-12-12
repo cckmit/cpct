0c7ddd43d092e8c456b950d473ef8df3626aa43d
linchao <1102171559@qq.com>
2018-12-12 18:31:38 +0800

    活动修改

--- cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TarGrpServiceImpl.java
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TarGrpServiceImpl.java
@@ -291,6 +291,7 @@ public class TarGrpServiceImpl extends BaseService implements TarGrpService {
             }
             if (conditionList.isEmpty()){
                 tarGrpMapper.deleteByPrimaryKey(tarGrpId);
+                mktCamGrpRulMapper.deleteByTarGrpId(tarGrpId);
                 redisUtils.remove("TAR_GRP_"+tarGrp.getTarGrpId());
             }
         } catch (Exception e) {
