6d2d9be366a4cc5d61ad835e8b8398a2a3cb1eac
gqa <657182420@qq.com>
2018-12-28 20:03:02 +0800

    '2'

--- cpct-count/src/main/java/com/zjtelcom/cpct/count/controller/GroupApiController.java
+++ cpct-count/src/main/java/com/zjtelcom/cpct/count/controller/GroupApiController.java
@@ -1,9 +1,15 @@
 package com.zjtelcom.cpct.count.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.zjtelcom.cpct.constants.CommonConstant;
+import com.zjtelcom.cpct.count.service.api.GroupApiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Auther: anson
@@ -15,6 +21,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupApiController {
 
     private Logger log = LoggerFactory.getLogger(GroupApiController.class);
+
+    @Autowired
+    private GroupApiService groupApiService;
+
+
+    @PostMapping("group")
+    @CrossOrigin
+    public String singleEvent(@RequestBody Map<String, Object> params) {
+        Map<String, Object> map = new HashMap<>();
+        try {
+            map = groupApiService.groupTrial(params);
+        } catch (Exception e) {
+            map.put("resultCode", CommonConstant.CODE_FAIL);
+            map.put("resultMsg", e.getMessage());
+        }
+        return JSON.toJSONString(map);
+    }
 
 
 
---
+++ cpct-count/src/main/java/com/zjtelcom/cpct/count/serviceImpl/api/GroupApiServiceImpl.java
@@ -0,0 +1,117 @@
+package com.zjtelcom.cpct.count.serviceImpl.api;
+
+import com.zjtelcom.cpct.count.controller.GroupApiController;
+import com.zjtelcom.cpct.count.service.api.GroupApiService;
+import com.zjtelcom.cpct.dao.grouping.TarGrpMapper;
+import com.zjtelcom.cpct.dto.grouping.TarGrp;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Auther: anson
+ * @Date: 2018/12/28
+ * @Description:分群id试算标签
+ */
+public class GroupApiServiceImpl implements GroupApiService {
+
+    private Logger log = LoggerFactory.getLogger(GroupApiServiceImpl.class);
+
+    @Autowired
+    private TarGrpMapper tarGrpMapper;
+
+
+    /**
+     * 通过分群id
+     * @param paramMap
+     * @return
+     */
+    @Override
+    public Map<String, Object> groupTrial(Map<String, Object> paramMap) {
+        Map<String, Object> map=new HashMap<>();
+        map.put("resultCode","1");
+        log.info("分群请求参数："+map);
+        String groupId = (String) map.get("groupId");
+        if(StringUtils.isBlank(groupId)){
+            map.put("resultCode","1000");
+            map.put("resultMsg","分群id信息不能为空");
+            return  map;
+        }
+        //分割字符串判断分群信息是否存在
+        List<String> groupList=getGroupList(groupId);
+        //依次搜索分群id信息是否存在
+        for (String str:groupList){
+            TarGrp tarGrp = tarGrpMapper.selectByPrimaryKey(Long.valueOf(str));
+            if(tarGrp==null){
+                map.put("resultCode","1000");
+                map.put("resultMsg","分群id "+str+" 信息不存在");
+                return  map;
+            }
+        }
+        //调用es的试算服务
+
+
+
+
+
+        log.info("试算返回信息");
+        return map;
+    }
+
+
+
+
+
+    /**
+     * 得到分群id集合
+     * @param groupId
+     * @return
+     */
+    public List<String> getGroupList(String groupId){
+        List<String> groupList=new ArrayList<>();
+        if(groupId.contains(";")){
+            String[] split = groupId.split(";");
+            for (String s:split){
+                groupList.add(s);
+            }
+        }else if(groupId.contains(",")){
+            String[] split = groupId.split(",");
+            for (String s:split){
+                groupList.add(s);
+            }
+        }else{
+            groupList.add(groupId);
+        }
+
+
+
+        return groupList;
+
+    }
+
+
+    /**
+     * 验证判断身份权限
+     * @param paramMap
+     * @return
+     */
+    public Map<String,String> verification(Map<String, Object> paramMap){
+        Map<String,String> map=new HashMap<>();
+        String channel = (String) paramMap.get("channel");
+        String channelToken = (String) paramMap.get("channelToken");
+
+
+        return map;
+
+    }
+
+
+
+
+}
