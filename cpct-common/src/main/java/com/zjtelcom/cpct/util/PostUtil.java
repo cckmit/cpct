05238d5863b34cc8bda4cbe1cc770044b9653d5f
gqa <657182420@qq.com>
2019-01-11 15:44:37 +0800

    Merge remote-tracking branch 'remotes/origin/dev_20181219' into dev_cpct_20181022_gqa_svn
    # Conflicts:
    #	cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/channel/ProductServiceImpl.java
    #	cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/event/ContactEvtItemServiceImpl.java

---
+++ cpct-common/src/main/java/com/zjtelcom/cpct/util/PostUtil.java
@@ -0,0 +1,52 @@
+package com.zjtelcom.cpct.util;
+
+import com.ctzj.smt.bss.sysmgr.model.common.Page;
+import com.ctzj.smt.bss.sysmgr.model.common.SysmgrResultObject;
+import com.ctzj.smt.bss.sysmgr.model.dataobject.SystemPost;
+import com.ctzj.smt.bss.sysmgr.model.query.QrySystemPostReq;
+import com.ctzj.smt.bss.sysmgr.privilege.service.dubbo.api.ISystemPostDubboService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+/**
+ * @Description:
+ * @author: linchao
+ * @date: 2019/01/07 16:25
+ * @version: V1.0
+ */
+@Component
+public class PostUtil {
+
+    private ISystemPostDubboService iSystemPostDubboService;
+
+    private static PostUtil postUtil;
+
+    @PostConstruct
+    public void init() {
+        postUtil= this;
+        postUtil.iSystemPostDubboService = iSystemPostDubboService;
+    }
+
+
+    public static String getPostNameByCode(String postCode){
+        String postName = "";
+        SystemPost systemPost = new SystemPost();
+        systemPost.setSysPostCode(postCode);
+        QrySystemPostReq qrySystemPostReq = new QrySystemPostReq();
+        qrySystemPostReq.setSystemPost(systemPost);
+        SysmgrResultObject<Page> pageSysmgrResultObject = postUtil.iSystemPostDubboService.qrySystemPostPage(new Page(), qrySystemPostReq);
+        if(pageSysmgrResultObject!=null){
+            if( pageSysmgrResultObject.getResultObject()!=null){
+                List<SystemPost> dataList = (List<SystemPost>) pageSysmgrResultObject.getResultObject().getDataList();
+                if(dataList!=null){
+                   if(dataList.get(0)!=null){
+                       postName = dataList.get(0).getSysPostName();
+                   }
+                }
+            }
+        }
+        return postName;
+    }
+}
