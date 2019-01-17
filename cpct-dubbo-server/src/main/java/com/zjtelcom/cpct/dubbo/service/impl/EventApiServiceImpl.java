76c78374cee281d019077e9061abafb079512849
11470847@qq.com <11470847@qq.com>
2019-01-17 21:11:06 +0800

    Merge branch 'dev_20181219_gqa_count' of https://gitee.com/cpct_admin/CPCT into dev_20181219_gqa_count
    # Conflicts:
    #	cpct-count/src/main/java/com/zjtelcom/cpct/count/serviceImpl/api/GroupApiServiceImpl.java

--- cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/impl/EventApiServiceImpl.java
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/impl/EventApiServiceImpl.java
@@ -822,6 +822,9 @@ public class EventApiServiceImpl implements EventApiService {
         public Map<String, Object> call() {
             Map<String, Object> activity = new HashMap<>();
 
+            long begin = System.currentTimeMillis();
+            System.out.println(activityId + "活动1111**************************" + (System.currentTimeMillis() - begin));
+
             //初始化es log
             JSONObject esJson = new JSONObject();
 
@@ -895,6 +898,7 @@ public class EventApiServiceImpl implements EventApiService {
             //iSale展示列参数对象初始化
             List<Map<String, Object>> itgTriggers = new ArrayList<>();
 
+            System.out.println(activityId + "活动2222**************************" + (System.currentTimeMillis() - begin));
             //验证过滤规则 活动级
             mktCampaign = (MktCampaignDO) redisUtils.get("MKT_" + activityId);
             List<Long> filterRuleIds = (List<Long>) redisUtils.get("MKT_FILTER_RULE_IDS" + activityId);
@@ -911,6 +915,7 @@ public class EventApiServiceImpl implements EventApiService {
                 }
             }
 
+            System.out.println(activityId + "活动3333**************************" + (System.currentTimeMillis() - begin));
             if (filterRuleIds != null && filterRuleIds.size() > 0) {
                 //循环并判断过滤规则
                 for (Long filterRuleId : filterRuleIds) {
@@ -1029,6 +1034,8 @@ public class EventApiServiceImpl implements EventApiService {
                 }
             }
 
+            System.out.println(activityId + "活动4444**************************" + (System.currentTimeMillis() - begin));
+
             //初始化结果集
             List<Future<Map<String, Object>>> threadList = new ArrayList<>();
             //初始化线程池
@@ -1054,6 +1061,10 @@ public class EventApiServiceImpl implements EventApiService {
                 //判断是否有策略命中
                 if (strategyList.size() > 0) {
                     esJson.put("hit", true); //添加命中标识
+
+
+                    System.out.println(activityId + "活动5555**************************" + (System.currentTimeMillis() - begin));
+
 
                     Map<String, Object> itgTrigger;
                     StringBuilder queryFieldsCust = new StringBuilder();
@@ -1194,6 +1205,10 @@ public class EventApiServiceImpl implements EventApiService {
                             map.put("triggers", JSONArray.parse(JSONArray.toJSON(evtTriggers).toString()));
                         }
                     }
+
+                    System.out.println(activityId + "活动6666**************************" + (System.currentTimeMillis() - begin));
+
+
                 } else {
                     esJson.put("hit", false);
                     esJson.put("msg", "策略均未命中");
@@ -1242,6 +1257,9 @@ public class EventApiServiceImpl implements EventApiService {
             //获取当前时间
             Map<String, Object> strategyMap = new HashMap<>();
 
+            long begin = System.currentTimeMillis();
+            System.out.println(strategyConfName + "策略1111**************************" + (System.currentTimeMillis() - begin));
+
             //初始化es log
             JSONObject esJson = new JSONObject();
 
@@ -1263,6 +1281,9 @@ public class EventApiServiceImpl implements EventApiService {
             esJson.put("integrationId", params.get("integrationId"));
             esJson.put("accNbr", params.get("accNbr"));
             esJson.put("hitEntity", privateParams.get("accNbr")); //命中对象
+
+            System.out.println(strategyConfName + "策略2222**************************" + (System.currentTimeMillis() - begin));
+
 
             //获取策略下所有标签
             List<Map<String, String>> allLabel = (List<Map<String, String>>) redisUtils.get("STRATEGY_ALL_LABEL_" + strategyConfId);
@@ -1300,6 +1321,9 @@ public class EventApiServiceImpl implements EventApiService {
             if (isSale) {
                 queryFieldsAss.append("PROM_INTEG_ID,");  //这里的逗号是有用的
             }
+
+            System.out.println(strategyConfName + "策略3333**************************" + (System.currentTimeMillis() - begin));
+
 
             DefaultContext<String, Object> context = new DefaultContext<String, Object>();
             //资产级标签
@@ -1339,6 +1363,9 @@ public class EventApiServiceImpl implements EventApiService {
                 }
             }
 
+            System.out.println(strategyConfName + "策略4444**************************" + (System.currentTimeMillis() - begin));
+
+
             //客户级标签
             if (queryFieldsCust.length() > 0) {
                 queryFieldsCust.deleteCharAt(queryFieldsCust.length() - 1);
@@ -1370,6 +1397,10 @@ public class EventApiServiceImpl implements EventApiService {
                     }
                 }
             }
+
+
+            System.out.println(strategyConfName + "策略5555**************************" + (System.currentTimeMillis() - begin));
+
 
             //销售品级标签
             if (queryFieldsSale.length() > 0) {
@@ -1411,6 +1442,8 @@ public class EventApiServiceImpl implements EventApiService {
                     }
                 }
             }
+
+            System.out.println(strategyConfName + "策略6666**************************" + (System.currentTimeMillis() - begin));
 
             context.putAll(labelItems);
 
@@ -1471,6 +1504,9 @@ public class EventApiServiceImpl implements EventApiService {
                 return Collections.EMPTY_MAP;
             }
 
+            System.out.println(strategyConfName + "策略7777**************************" + (System.currentTimeMillis() - begin));
+
+
             esHitService.save(esJson, IndexList.STRATEGY_MODULE);
             return strategyMap;
         }
@@ -1512,6 +1548,10 @@ public class EventApiServiceImpl implements EventApiService {
 
         @Override
         public Map<String, Object> call() throws Exception {
+
+            long begin = System.currentTimeMillis();
+            System.out.println(ruleName + "规则1111**************************" + (System.currentTimeMillis() - begin));
+
 
             //初始化es log   标签使用
             JSONObject esJson = new JSONObject();
@@ -1560,6 +1600,9 @@ public class EventApiServiceImpl implements EventApiService {
             //记录实例不足的标签
             StringBuilder notEnoughLabel = new StringBuilder();
 
+            System.out.println(ruleName + "规则2222**************************" + (System.currentTimeMillis() - begin));
+
+
             //判断表达式在缓存中有没有
             String express = (String) redisUtils.get("EXPRESS_" + tarGrpId);
 //            String express = "";
@@ -1594,8 +1637,13 @@ public class EventApiServiceImpl implements EventApiService {
                     expressSb.append("if(");
                     //遍历所有规则
                     for (Map<String, String> labelMap : labelMapList) {
-
                         String type = labelMap.get("operType");
+
+                        if ("PROM_LIST".equals(labelMap.get("code"))) {
+                            FilterRule filterRule = filterRuleMapper.selectByPrimaryKey(Long.valueOf(labelMap.get("rightParam")));
+                            labelMap.put("rightParam", filterRule.getChooseProduct());
+                        }
+
                         //保存标签的es log
                         lr = new LabelResult();
                         lr.setOperType(type);
@@ -1711,6 +1759,9 @@ public class EventApiServiceImpl implements EventApiService {
                 esJson.put("labelResultList", JSONArray.toJSON(labelResultList));
             }
 
+            System.out.println(ruleName + "规则3333**************************" + (System.currentTimeMillis() - begin));
+
+
             esHitService.save(esJson, IndexList.Label_MODULE);
 
             //验证是否标签实例不足
@@ -1726,8 +1777,13 @@ public class EventApiServiceImpl implements EventApiService {
                 RuleResult ruleResult = null;
                 ExpressRunner runnerQ = new ExpressRunner();
                 runnerQ.addFunction("toNum", new StringToNumOperator("toNum"));
+                runnerQ.addFunction("checkProm", new PromCheckOperator("checkProm"));
                 try {
                     ruleResult = runnerQ.executeRule(express, context, true, true);
+
+                    System.out.println(ruleName + "规则4444**************************" + (System.currentTimeMillis() - begin));
+
+
                 } catch (Exception e) {
                     ruleMap.put("msg", "规则引擎计算失败");
 
@@ -2273,6 +2329,11 @@ public class EventApiServiceImpl implements EventApiService {
     }
 
 
+    /**
+     * 规则引擎过滤：字符串转数字 （用于大小区间比较）
+     * <p>
+     * 参数：待比较字段
+     */
     class StringToNumOperator extends Operator {
         public StringToNumOperator(String name) {
             this.name = name;
@@ -2285,6 +2346,66 @@ public class EventApiServiceImpl implements EventApiService {
             } else {
                 return str;
             }
+        }
+    }
+
+    /**
+     * 规则引擎过滤：销售品标签过滤
+     * <p>
+     * 参数：已办理销售品、类型、过滤规则配置销售品
+     */
+    class PromCheckOperator extends Operator {
+        public PromCheckOperator(String name) {
+            this.name = name;
+        }
+
+        public Object executeInner(Object[] list) throws Exception {
+            boolean productCheck = true;
+            //获取用户已办理销售品
+            String productStr = (String) list[0];
+            //获取过滤类型
+            String type = (String) list[1];
+            //过滤规则配置的销售品
+            String checkProduct = (String) list[2];
+            //获取需要过滤的销售品
+            if (checkProduct != null && !"".equals(checkProduct)) {
+                String[] checkProductArr = checkProduct.split(",");
+                if (productStr != null && !"".equals(productStr)) {
+                    if ("1000".equals(type)) {  //存在于
+                        for (String product : checkProductArr) {
+                            int index = productStr.indexOf(product);
+                            if (index >= 0) {
+                                productCheck = false;
+                                break;
+                            }
+                        }
+                    } else if ("2000".equals(type)) { //不存在于
+                        boolean noExistCheck = true;
+                        for (String product : checkProductArr) {
+                            int index = productStr.indexOf(product);
+                            if (index >= 0) {
+                                productCheck = true;
+                                noExistCheck = false;
+                                //被过滤的销售品
+                                break;
+                            }
+                        }
+                        if (noExistCheck) {
+                            productCheck = false;
+                        }
+                    }
+                } else {
+                    //存在于校验
+                    if ("2000".equals(type)) {
+                        productCheck = false;
+                    } else if ("1000".equals(type)) {
+                        productCheck = true;
+                    }
+                }
+            }
+
+            return productCheck;
+
         }
     }
 
@@ -2304,13 +2425,18 @@ public class EventApiServiceImpl implements EventApiService {
 
     public static String cpcExpression(String code, String type, String rightParam) {
         StringBuilder express = new StringBuilder();
-
-        if ("7100".equals(type)) {
-            express.append("!");
+        if ("PROM_LIST".equals(code)) {
+            express.append("checkProm(").append(code).append(",").append("type").append(rightParam);
+            express.append("))");
+        } else {
+            if ("7100".equals(type)) {
+                express.append("!");
+            }
+            express.append("((");
+            express.append(assLabel(code, type, rightParam));
+            express.append(")");
         }
-        express.append("((");
-        express.append(assLabel(code, type, rightParam));
-        express.append(")");
+
 
         return express.toString();
     }
@@ -2332,6 +2458,7 @@ public class EventApiServiceImpl implements EventApiService {
 
     public static String assLabel(String code, String type, String rightParam) {
         StringBuilder express = new StringBuilder();
+
         switch (type) {
             case "1000":
                 express.append("toNum(").append(code).append("))");
@@ -2384,8 +2511,9 @@ public class EventApiServiceImpl implements EventApiService {
                 express.append(" && ").append("(toNum(");
                 express.append(code).append("))");
                 express.append(" <= ").append(strArray2[1]);
-
+                break;
         }
+
         return express.toString();
     }
 
