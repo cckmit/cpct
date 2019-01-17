acb3f5af6bae77a89f938d4f7ba1aaf534ba7970
gqa <657182420@qq.com>
2019-01-17 17:27:52 +0800

    Merge remote-tracking branch 'remotes/origin/dev_20181219_gqa_count' into dev_cpct_1212_svn_config_new
    # Conflicts:
    #	cpct-dubbo-server/src/main/resources/application-pre.yml

--- cpct-common/src/main/java/com/zjtelcom/cpct/dao/filter/FilterRuleMapper.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dao/filter/FilterRuleMapper.java
@@ -38,6 +38,6 @@ public interface FilterRuleMapper {
 
     List<FilterRule> selectByProduct(@Param("chooseProduct") String chooseProduct);
 
-    List<FilterRule> selectFilterRuleListByStrategyId(@Param("strategyId") Long strategyId);
+    List<FilterRule> selectFilterRuleListByStrategyId(@Param("strategyId") Long strategyId,@Param("strategyType") String strategyType);
 
 }
