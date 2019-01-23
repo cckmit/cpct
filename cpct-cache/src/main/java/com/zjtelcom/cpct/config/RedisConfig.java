5042aba1413e3069a123335a3614938087c1c624
11470847@qq.com <11470847@qq.com>
2019-01-23 14:26:13 +0800

    Merge branch 'dev_20181219_gqa_count' of https://gitee.com/cpct_admin/CPCT into dev_20181219_gqa_count
    # Conflicts:
    #	cpct-count/src/main/java/com/zjtelcom/cpct/count/serviceImpl/api/GroupApiServiceImpl.java

--- cpct-cache/src/main/java/com/zjtelcom/cpct/config/RedisConfig.java
+++ cpct-cache/src/main/java/com/zjtelcom/cpct/config/RedisConfig.java
@@ -116,10 +116,10 @@ public class RedisConfig extends CachingConfigurerSupport {
         hostAndPortList.add(host);
 
         GenericObjectPoolConfig poolConfig = new JedisPoolConfig();
-        poolConfig.setMaxIdle(5); //最大空闲连接数
-        poolConfig.setMaxTotal(10); // 最大连接数（空闲+使用中），不超过应用线程数，建议为应用线程数的一半
-        poolConfig.setMinIdle(5); //保持的最小空闲连接数
-        poolConfig.setMaxWaitMillis(3000);
+        poolConfig.setMaxIdle(50); //最大空闲连接数
+        poolConfig.setMaxTotal(500); // 最大连接数（空闲+使用中），不超过应用线程数，建议为应用线程数的一半
+        poolConfig.setMinIdle(10); //保持的最小空闲连接数
+        poolConfig.setMaxWaitMillis(2000);
         CtgJedisPoolConfig config = new CtgJedisPoolConfig(hostAndPortList);
         config.setDatabase(redisDataBase).setPassword(redisPassword).setPoolConfig(poolConfig).setPeriod(1000).setMonitorTimeout(100);
 
