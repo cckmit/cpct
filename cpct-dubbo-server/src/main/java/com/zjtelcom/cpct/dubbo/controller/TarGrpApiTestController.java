3dbfcf0836a1b35e5cb5956da8f063fc8dd80c64
linchao <1102171559@qq.com>
2019-04-28 21:03:15 +0800

    事件接入问题修复以及性能优化

--- cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/controller/TarGrpApiTestController.java
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/controller/TarGrpApiTestController.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/tarGrpTest")
+@RequestMapping("${adminPath}/tarGrpTest")
 public class TarGrpApiTestController {
 
     @Autowired
