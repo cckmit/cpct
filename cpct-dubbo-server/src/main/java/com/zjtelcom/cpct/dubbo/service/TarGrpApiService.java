9f7d3986bbad8acaf63e012c17c7acfab2eecac1
linchao <1102171559@qq.com>
2019-04-28 16:16:32 +0800

    1. 事件接入添加销售品过滤 2. 添加客户分群dubbo接口

---
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/TarGrpApiService.java
@@ -0,0 +1,15 @@
+package com.zjtelcom.cpct.dubbo.service;
+
+import java.util.Map;
+
+/**
+ * @Description:
+ * @author: linchao
+ * @date: 2019/04/22 14:10
+ * @version: V1.0
+ */
+public interface TarGrpApiService {
+
+    Map<String, Object> getCpcTargrp(Map<String, Object> paramsMap);
+
+}
