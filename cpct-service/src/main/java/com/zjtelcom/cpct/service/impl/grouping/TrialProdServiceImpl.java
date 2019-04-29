a9ee6e1b6bc125a589fe5ca2f1c73441eac7e8c2
侯云峰 <992624275@qq.com>
2019-04-29 15:31:34 +0800

    es

--- cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TrialProdServiceImpl.java
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/grouping/TrialProdServiceImpl.java
@@ -144,8 +144,11 @@ public class TrialProdServiceImpl implements TrialProdService {
     public void campaignIndexTask(Map<String,Object> param) {
         List<MktCampaignDO> campaignList = new ArrayList<>();
         Long campaignId = MapUtil.getLongNum(param.get("id"));
-        MktCampaignDO cam = campaignMapper.selectByPrimaryKey(campaignId);
-        campaignList.add(cam);
+        List<Integer> idList = ( List<Integer>)param.get("idList");
+        for (Integer id : idList){
+            MktCampaignDO cam = campaignMapper.selectByPrimaryKey(Long.valueOf(id.toString()));
+            campaignList.add(cam);
+        }
         for (MktCampaignDO campaignDO : campaignList){
             if(!StatusCode.STATUS_CODE_PUBLISHED.getStatusCode().equals(campaignDO.getStatusCd())){
                continue;
@@ -163,6 +166,7 @@ public class TrialProdServiceImpl implements TrialProdService {
                 operation.setStrategyId(strategy.getMktStrategyConfId());
                 operation.setStrategyName(strategy.getMktStrategyConfName());
                 operation.setBatchNum(Long.valueOf(batchNumSt));
+                trialOperationMapper.insert(operation);
                 Map<String,Object> res = issue(operation,campaignDO,strategy);
                 System.out.println(JSON.toJSONString(res));
             }
