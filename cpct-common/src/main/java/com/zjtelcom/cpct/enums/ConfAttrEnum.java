1395471b1bbeb6defc3e026d05cf760fa2f335fa
侯云峰 <992624275@qq.com>
2019-06-13 16:19:09 +0800

    es

--- cpct-common/src/main/java/com/zjtelcom/cpct/enums/ConfAttrEnum.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/enums/ConfAttrEnum.java
@@ -28,8 +28,8 @@ public enum ConfAttrEnum {
     ACCOUNT(500600010012L, "接触账号"),
     ISEE_CUSTOMER(500600010019L, "ISEE派单到人"),
     ISEE_AREA(500600010020L, "ISEE派单到区域"),
-    ISEE_LABEL_CUSTOMER(500600010021L, "ISEE标签选择"),
-    ISEE_LABEL_AREA(500600010022L, "ISEE标签选择");
+    ISEE_LABEL_CUSTOMER(500600010021L, "ISEE标签选择派单人"),
+    ISEE_LABEL_AREA(500600010022L, "ISEE标签选择区域");
 
     private Long arrId;
     private String arrName;
