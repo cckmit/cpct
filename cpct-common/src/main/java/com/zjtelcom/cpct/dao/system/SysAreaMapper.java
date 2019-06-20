a7110ad76cbbb287f90e4159442e08becd6d996e
x <1060250502@qq.com>
2019-06-20 16:38:30 +0800

    获取组织树接口修改

--- cpct-common/src/main/java/com/zjtelcom/cpct/dao/system/SysAreaMapper.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dao/system/SysAreaMapper.java
@@ -22,4 +22,8 @@ public interface SysAreaMapper {
     List<SysArea> selectByParnetArea(Integer parentArea);
 
     int updateByPrimaryKey(SysArea record);
+
+    List<SysArea> getByAreaLevelOne(Long regionIdByLandId);
+
+    List<SysArea> getByParentArea(Long regionIdByLandId);
 }
