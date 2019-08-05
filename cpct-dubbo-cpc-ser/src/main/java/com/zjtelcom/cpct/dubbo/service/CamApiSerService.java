ecba89914141bdd956705cb4dc600b380a328b9e
侯云峰 <992624275@qq.com>
2019-08-05 10:32:02 +0800

    123

---
+++ cpct-dubbo-cpc-ser/src/main/java/com/zjtelcom/cpct/dubbo/service/CamApiSerService.java
@@ -0,0 +1,12 @@
+package com.zjtelcom.cpct.dubbo.service;
+
+import com.ql.util.express.DefaultContext;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CamApiSerService {
+
+    Map<String, Object>  ActivitySerTask(Map<String, String> params, Long activityId, Map<String, String> privateParams, Map<String, String> labelItems, List<Map<String, Object>> evtTriggers, List<Map<String, Object>> strategyMapList, DefaultContext<String, Object> context);
+
+}
