14f936a78b9e51c36fdb5b06b9f437e32675a540
15067282769@163.com <15067282769@163.com>
2019-09-25 19:28:56 +0800

    1111

--- cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/out/TargetGroupService.java
+++ cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/out/TargetGroupService.java
@@ -2,7 +2,7 @@ package com.zjtelcom.cpct.dubbo.out;
 
 import java.util.Map;
 
-public interface TarGrpService {
+public interface TargetGroupService {
 
     Map<String, Object> tarGrpTemplateScheduledBatchIssue();
 }
