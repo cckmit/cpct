f4a773bdb71ac9494757a5c91e69e28d25d1bec4
15067282769@163.com <15067282769@163.com>
2019-09-26 16:04:24 +0800

    1111

--- cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/controller/SyncLabelController.java
+++ cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/controller/SyncLabelController.java
@@ -2,22 +2,27 @@ package com.zjtelcom.cpct.dubbo.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.zjtelcom.cpct.dubbo.model.RecordModel;
+import com.zjtelcom.cpct.dubbo.out.CampaignService;
 import com.zjtelcom.cpct.dubbo.service.SyncEventService;
 import com.zjtelcom.cpct.dubbo.service.SyncLabelService;
+import com.zjtelcom.cpct.util.DateUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/label")
 public class SyncLabelController {
-    @Autowired
-    private SyncLabelService syncLabelService;
 
     @Autowired
+    private SyncLabelService syncLabelService;
+    @Autowired
     private SyncEventService syncEventService;
+    @Autowired
+    private CampaignService campaignService;
 
     @RequestMapping(value = "syncLabel", method = RequestMethod.POST)
     @CrossOrigin
@@ -43,5 +48,13 @@ public class SyncLabelController {
         return "调用成功";
     }
 
+    @PostMapping("campaignDelayNotice")
+    @CrossOrigin
+    public void campaignDelayNotice() {
+        /*String endTime = "2019-10-02";
+        Date date = DateUtil.string2DateTime4Minute(endTime);
+        int i = DateUtil.daysBetween(new Date(), date);*/
 
+        campaignService.campaignDelayNotice();
+    }
 }
