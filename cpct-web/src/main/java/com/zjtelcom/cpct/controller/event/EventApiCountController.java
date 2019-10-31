3a283d62cba20f1ba85c3ce27f96ae9f33f69d74
linchao <1102171559@qq.com>
2019-10-31 17:27:07 +0800

    1.dubbo对内开发 2.cpc计算开发

--- cpct-web/src/main/java/com/zjtelcom/cpct/controller/event/EventApiCountController.java
+++ cpct-web/src/main/java/com/zjtelcom/cpct/controller/event/EventApiCountController.java
@@ -3,13 +3,12 @@ package com.zjtelcom.cpct.controller.event;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.zjtelcom.cpct.controller.BaseController;
-import com.zjtelcom.cpct.request.event.QryContactEvtTypeReq;
 import com.zjtelcom.cpct.service.event.EventApiCountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,15 +19,13 @@ import java.util.Map;
  * @date: 2019/10/29 20:19
  * @version: V1.0
  */
-@Controller
+@RestController
 @RequestMapping("${adminPath}/eventApiCount")
 public class EventApiCountController extends BaseController {
 
     @Autowired
     private EventApiCountService eventApiCountService;
-    /**
-     * 查询事件目录树
-     */
+
     @RequestMapping("/count")
     @CrossOrigin
     public String eventApiCount(@RequestBody Map<String, Object> param) {
