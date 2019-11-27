c6cc82f14430984ae6ae434831638a8d03f31232
15067282769@163.com <15067282769@163.com>
2019-11-27 22:16:55 +0800

    11

--- cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/campaign/MktCamChlConfServiceImpl.java
+++ cpct-service/src/main/java/com/zjtelcom/cpct/service/impl/campaign/MktCamChlConfServiceImpl.java
@@ -233,10 +233,41 @@ public class MktCamChlConfServiceImpl extends BaseService implements MktCamChlCo
         try {
             MktCamChlConfDO mktCamChlConfDO = mktCamChlConfMapper.selectByPrimaryKey(evtContactConfId);
             List<MktCamChlConfAttrDO> mktCamChlConfAttrDOList = mktCamChlConfAttrMapper.selectByEvtContactConfId(evtContactConfId);
-            for (MktCamChlConfAttrDO mktCamChlConfAttrDO : mktCamChlConfAttrDOList) {
-                if (mktCamChlConfAttrDO.getAttrId() == null) {
-                    Long contactChlAttrRstrId = mktCamChlConfAttrDO.getContactChlAttrRstrId();
-                    mktCamChlConfAttrMapper.deleteByPrimaryKey(contactChlAttrRstrId);
+            // 集团电渠下发集团活动渠道属性缺失特殊逻辑弥补
+            if (3969983L == mktCamChlConfDO.getContactChlId()){
+                List<Long> list =  new ArrayList<>();
+                for (MktCamChlConfAttrDO mktCamChlConfAttrDO : mktCamChlConfAttrDOList) {
+                    if (mktCamChlConfAttrDO.getAttrId() == null) {
+                        Long contactChlAttrRstrId = mktCamChlConfAttrDO.getContactChlAttrRstrId();
+                        mktCamChlConfAttrMapper.deleteByPrimaryKey(contactChlAttrRstrId);
+                    }else{
+                        list.add(mktCamChlConfAttrDO.getAttrId());
+                    }
+                }
+                MktCamChlConfAttrDO mktCamChlConfAttrDO = new MktCamChlConfAttrDO();
+                mktCamChlConfAttrDO.setEvtContactConfId(mktCamChlConfDO.getEvtContactConfId());
+                mktCamChlConfAttrDO.setAttrValue("");
+                mktCamChlConfAttrDO.setAttrValueId(0L);
+                mktCamChlConfAttrDO.setCreateDate(new Date());
+                mktCamChlConfAttrDO.setCreateStaff(UserUtil.loginId());
+                mktCamChlConfAttrDO.setUpdateDate(new Date());
+                mktCamChlConfAttrDO.setUpdateStaff(UserUtil.loginId());
+                mktCamChlConfAttrDO.setStatusCd(StatusCode.STATUS_CODE_EFFECTIVE.getStatusCode());
+                mktCamChlConfAttrDO.setStatusDate(new Date());
+                if (!list.contains(500600010006L)) {
+                    mktCamChlConfAttrDO.setAttrId(500600010006L);
+                    mktCamChlConfAttrDO.setContactChlAttrRstrId(null);
+                    mktCamChlConfAttrMapper.insert(mktCamChlConfAttrDO);
+                }
+                if (!list.contains(500600010007L)) {
+                    mktCamChlConfAttrDO.setAttrId(500600010007L);
+                    mktCamChlConfAttrDO.setContactChlAttrRstrId(null);
+                    mktCamChlConfAttrMapper.insert(mktCamChlConfAttrDO);
+                }
+                if (!list.contains(500600010012L)) {
+                    mktCamChlConfAttrDO.setAttrId(500600010012L);
+                    mktCamChlConfAttrDO.setContactChlAttrRstrId(null);
+                    mktCamChlConfAttrMapper.insert(mktCamChlConfAttrDO);
                 }
             }
             mktCamChlConfAttrDOList = mktCamChlConfAttrMapper.selectByEvtContactConfId(evtContactConfId);
