ff7eed8975efd22487d971e5b47836c24d7444d3
15067282769@163.com <15067282769@163.com>
2019-12-05 14:53:19 +0800

    1111

--- cpct-common/src/main/java/com/zjtelcom/cpct/dao/event/ContactEvtMapper.java
+++ cpct-common/src/main/java/com/zjtelcom/cpct/dao/event/ContactEvtMapper.java
@@ -59,5 +59,7 @@ public interface ContactEvtMapper {
 
     List<EventInterfaceRel> selectEvtInterfaceRelByEvtId(Long evtId);
 
+    EventInterfaceRel selectEvtInterfaceRel(EventInterfaceRel eventInterfaceRel);
+
     List<String> selectChannelListByEvtId(Long evtId);
 }
