bf48f92f9777707315a13775a1a4ef9534205c4b
x <1060250502@qq.com>
2019-12-30 10:51:49 +0800

    老报表修改使用initID查询活动

--- cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/impl/EventApiServiceImpl.java
+++ cpct-dubbo-server/src/main/java/com/zjtelcom/cpct/dubbo/service/impl/EventApiServiceImpl.java
@@ -31,6 +31,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -38,6 +39,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
+@Transactional
 public class EventApiServiceImpl implements EventApiService {
 
     @Value("${thread.maxPoolSize}")
