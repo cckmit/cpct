4ff7371ab1428c62ebb4d9c3e6b62df7ec4f5abd
15067282769@163.com <15067282769@163.com>
2020-03-09 15:59:56 +0800

    1111

--- cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/out/impl/CampaignServiceImpl.java
+++ cpct-dubbo-label/src/main/java/com/zjtelcom/cpct/dubbo/out/impl/CampaignServiceImpl.java
@@ -2,6 +2,7 @@ package com.zjtelcom.cpct.dubbo.out.impl;
 
 import com.zjtelcom.cpct.dubbo.out.CampaignService;
 import com.zjtelcom.cpct.service.campaign.MktCampaignService;
+import com.zjtelcom.cpct.service.scheduled.ScheduledTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,8 @@ public class CampaignServiceImpl implements CampaignService {
 
     @Autowired
     private MktCampaignService campaignService;
+    @Autowired
+    private ScheduledTaskService scheduledTaskService;
 
     /**
      * 活动延期短信通知
@@ -18,5 +21,13 @@ public class CampaignServiceImpl implements CampaignService {
     @Override
     public void campaignDelayNotice() {
         campaignService.campaignDelayNotice();
+    }
+
+    /**
+     * 定时任务：每个派单成功活动取最后一个批次若该批次下发时间超过20天（可变），查询处理率若低于1%（可变)，则自动失效，并短信通知
+     */
+    @Override
+    public void issuedSuccessMktCheck() {
+        scheduledTaskService.issuedSuccessMktCheck();
     }
 }
