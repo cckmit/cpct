745f016bb119a70ab3825e8c01332b6227182048
linchao <975179269@qq.com>
2020-03-16 15:49:44 +0800

    策略中心控制层

---
+++ cpct-web/src/main/java/com/zjtelcom/cpct/controller/blacklist/BlackListController.java
@@ -0,0 +1,34 @@
+package com.zjtelcom.cpct.controller.blacklist;
+
+import com.zjtelcom.cpct.controller.BaseController;
+import com.zjtelcom.cpct.service.blacklist.BlackListService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("${adminPath}/blacklist")
+public class BlackListController extends BaseController {
+
+    private BlackListService blackListService;
+
+    /**
+     * 导出黑名单
+     *
+     * @return
+     */
+    @PostMapping("/export")
+    @CrossOrigin
+    public Map<String, Object> export(){
+        Map<String,Object> result = new HashMap<>();
+        try {
+            blackListService.exportBlackListFile();
+        } catch (Exception e) {
+            logger.error("[op:BlackListController] fail to exportBlackListFile",e);
+        }
+        return result;
+    }
+}
